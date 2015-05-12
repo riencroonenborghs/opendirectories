6f285a2a14934ebd08c1826fe8cc34bd56474552
Rien Croonenborghs <rien@croonenborghs.net>
2015-05-12 20:25:36 +1200

    added blacklist

--- chrome/opendirectories/resources/js/app.js
+++ chrome/opendirectories/resources/js/app.js
@@ -1,12 +1,14 @@
 var app = angular.module("opendirectories", ["ngAria", "ngAnimate", "ngMaterial", "ngMdIcons"]);
-app.controller("appController", ["$scope", "$mdMedia", function($scope, $mdMedia){
+var blacklist = ["watchtheshows.com", "mmnt.net", "listen77.com", "unknownsecret.info", "trimediacentral.com", "wallywashis.name", "ch0c.com"];
+
+app.controller("appController", ["$scope", "$mdMedia", "$mdSidenav", function($scope, $mdMedia, $mdSidenav){
   $scope.query = null;
   $scope.query_types = ["Movies", "Music", "Books"];
   $scope.query_type = $scope.query_types[0];
   $scope.alternative = false
   $scope.play_dumb = true
   
-  var server = "https://www.google.co.nz/";
+  var server = "https://www.google.com/";
   var path   = "search";
   var build_url  = function() { return server + path + "?q=" + encodeURIComponent($scope.build_query()); }
   $scope.build_query = function() {
@@ -16,19 +18,58 @@ app.controller("appController", ["$scope", "$mdMedia", function($scope, $mdMedia
     
     if($scope.play_dumb) { query_string += '"' + $scope.query + '"'; }
     else { query_string += $scope.query; }
-    query_string += ' -html -htm -php -asp -jsp -watchtheshows.com -mmnt.net -listen77.com -unknownsecret.info -trimediacentral.com -wallywashis.name -ch0c.com';
+    query_string += ' -html -htm -php -asp -jsp ';
+    for(var i=0; i<blacklist.length; ++i) { query_string += " -" + blacklist[i]; }
 
     var types = {"Movies": " (avi|mp4|divx) ", "Music": " (mp3|flac|aac) ", "Books": " (pdf|epub|mob) "};
     return query_string + types[$scope.query_type]; 
   }
 
   $scope.search = function() {
-    if($scope.query) {
+    if($scope.query) { 
       window.open(build_url(), "_blank");
     }          
+  }
+
+  $scope.show_menu = function() {
+    $mdSidenav('right').toggle()
   }
 
   $scope.$watch(function() { return $mdMedia('sm'); }, function(small) {
     $scope.small = small;
   });
+}]);
+
+
+app.controller("menuController", ["$scope", "$mdSidenav", function($scope, $mdSidenav){  
+  chrome.storage.sync.get('blacklist', function (data) {
+    if(data.blacklist) {      
+      blacklist = JSON.parse(data.blacklist);
+      $scope.blacklist = blacklist;
+    }
+  });
+   
+  $scope.blacklist = blacklist;
+  $scope.close = function() {
+     $mdSidenav('right').close();
+  }
+
+  $scope.url = null;
+  $scope.add = function() {
+    if($scope.url) {
+      blacklist.push($scope.url);
+      $scope.url = null;
+      store();
+    }
+  }
+
+  $scope.remove = function(item) {
+    blacklist.splice(blacklist.indexOf(item), 1);
+    store();
+  }
+
+  var store = function() {
+    var data = {}; data['blacklist'] = JSON.stringify(blacklist);
+    chrome.storage.sync.set(data);
+  }  
 }]);
