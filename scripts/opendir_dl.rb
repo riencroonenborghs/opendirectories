389639239b04de385327041ef5c6766d32022e96
Rien Croonenborghs <rien@croonenborghs.net>
2015-05-29 08:30:03 +1200

    no mnirror added

--- scripts/opendir_dl.rb
+++ scripts/opendir_dl.rb
@@ -7,11 +7,13 @@ opts = GetoptLong.new(
   [ '--password', '-p', GetoptLong::OPTIONAL_ARGUMENT ],
   [ '--output', '-o', GetoptLong::OPTIONAL_ARGUMENT ],
   [ '--files', '-f', GetoptLong::OPTIONAL_ARGUMENT ],
-  [ '--no-check-cert', '-n', GetoptLong::NO_ARGUMENT ],
+  [ '--no-check-cert', '-c', GetoptLong::NO_ARGUMENT ],
+  [ '--no-mirror', '-m', GetoptLong::NO_ARGUMENT ],
   [ '--help', '-h', GetoptLong::NO_ARGUMENT ]
 )
 
-http_user = http_password = output_directory = files = no_check_cert = help = nil
+http_user = http_password = output_directory = files = nil
+no_check_cert = no_mirror = help = false
 
 opts.each do |opt, arg|
   case opt
@@ -25,13 +27,15 @@ opts.each do |opt, arg|
     files = arg
   when '--no-check-cert'
     no_check_cert = true
+  when '--no-mirror'
+    no_mirror = true
   when '--help'
     help = true
   end
 end
 
 if help
-  puts "#{$0} [--user|-u http_username] [--password|-p http_password] [--output|-o output_directory] [--files|-f file_filter] [--no-check-cert|-n] [--help|-h] URL"
+  puts "#{$0} [--user|-u http_username] [--password|-p http_password] [--output|-o output_directory] [--files|-f file_filter] [--no-check-cert|-c] [--no-mirror|-m] [--help|-h] URL"
   exit 0
 end
 
@@ -42,7 +46,8 @@ end
 
 url = ARGV[0]
 
-cmd = "wget  -r -c -np -e robots=off --random-wait --reject \"index.html*\""
+cmd = "wget -c --reject \"index.html*\" "
+cmd += " -r -np -e robots=off --random-wait " unless no_mirror
 cmd += " --http-user=\"#{http_user}\" --http-password=\"#{http_password}\" " if http_user && http_password
 cmd += " --directory-prefix=\"#{output_directory}\" " if output_directory
 cmd += " --accept \"#{files}\" " if files
